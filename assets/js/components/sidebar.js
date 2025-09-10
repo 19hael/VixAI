1d693e87a6c67754497741e3f0d1abacf790d450
Hael <yoenqueco@gmail.com>
2025-09-09 18:15:31 -0600

    sidebar.js

--- assets/js/components/sidebar.js
+++ assets/js/components/sidebar.js
@@ -4,14 +4,14 @@ export function buildSidebar(){
   wrap.innerHTML = `
     <div class="brand">
       <img class="brand-logo" src="./IMG/LOGO ND D ANIMATION.png" alt="L"/>
-      <span>Lost Premium</span>
+      <span>HACKING PREMIUM SERVICES</span>
     </div>
     <nav>
       <a href="#/admin-panel">Admin Panel</a>
       <a href="#/administration">Administration</a>
-      <a href="#/functions">Functions</a>
-      <a href="#/h-functions">H Functions</a>
-      <a href="#/l-premium">L Premium</a>
+      <a href="#/functions">1337</a>
+      <a href="#/h-functions">TOOLS</a>
+      <a href="#/l-premium">HACKING PREMIUM TOOLS</a>
     </nav>
   `;
   return wrap;
